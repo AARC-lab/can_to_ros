e41c0edcc6207d001d75634c2a8d6b25fee5653a
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2024-09-30 16:20:40 -0500

    Preparing for Converting CSV to BAG by playing back using ROS locally

--- scripts/regenerateCanToRos.sh
+++ scripts/regenerateCanToRos.sh
@@ -5,25 +5,15 @@
 #     exit 2
 # fi
 
-LIBPANDA_USER=$(cat /etc/libpanda.d/libpanda_usr)
-FILE=/etc/libpanda.d/vin_details.json
-if test -f "$FILE"; then
-    echo "$FILE exists."
-else
-    echo "$FILE doesn't exist. Running vinParser"
-    sudo python3 /home/$LIBPANDA_USER/libpanda/scripts/vinParser.py
-    # python3 /Users/mnice/Documents/GitHub/libpanda_real/scripts/vinParser.py
-fi
-
-echo "The current vin details are  " $(cat /etc/libpanda.d/vin_details.json)
-echo "The current vin is " $(cat /etc/libpanda.d/vin)
+python vinParser.py
+echo "The current vin details are  " $(cat ./vin_details.json)
+echo "The current vin is " $(cat ./vin)
 echo "If something is incorrect, run libpanda/scripts/setVin.sh, or run vinParser.py to lookup details"
 
 read -r -p "Does this information look correct? Proceed to generating CANtoROS from this VIN? [Y/N] " response
   case "$response" in
       [yY][eE][sS]|[yY])
 
-	mkdir -p /home/$LIBPANDA_USER/strym/strym/dbc/
 
 	wget https://raw.githubusercontent.com/jmscslgroup/strym/master/strym/dbc/toyota_rav4_hybrid.dbc  -O ~/strym/strym/dbc/toyota_rav4_hybrid.dbc
 	wget https://raw.githubusercontent.com/jmscslgroup/strym/master/strym/dbc/honda_pilot_2017.dbc  -O ~/strym/strym/dbc/honda_pilot_2017.dbc
@@ -36,8 +26,5 @@ read -r -p "Does this information look correct? Proceed to generating CANtoROS f
 	python3 ./generateDecode_Subs.py
 	./regen_vehicle_interface.sh
 
-#	cd /home/$LIBPANDA_USER/catkin_ws
-#	source devel/setup.bash
-#	catkin_make
     echo "Code generation complete, please perform catkin_make"
 esac
